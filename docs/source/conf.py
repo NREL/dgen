3e3af4017f5f4830dc805b11e26ece261c4ddcf5
tstanley <Trevor.Stanley@nrel.gov>
2020-12-22 17:10:18 -0700

    changed sphinx theme format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,12 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../dgen_os'))
-#sys.path.insert(0, os.path.abspath('../dgen_os/python'))
+
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../dgen_os'))
 sys.path.insert(0, os.path.abspath('../../dgen_os/python'))
 
-#print('$$$$$$$', os.path.abspath('../..'))
-#sys.path.insert(0, os.path.abspath('../dgen_os'))
-#sys.path.insert(0, os.path.abspath('../../dgen_os/python'))
-# sys.path.insert(0, os.path.abspath('.'))
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -55,13 +50,27 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
