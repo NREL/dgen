df3f3819283f1660d95d6267887fafb6296b5ff2
tstanley <Trevor.Stanley@nrel.gov>
2021-07-08 09:51:38 -0600

    fixing merge conflict

--- dgen_os/python/input_data_functions.py
+++ dgen_os/python/input_data_functions.py
@@ -233,9 +233,9 @@ def import_table(scenario_settings, con, engine, role, input_name, csv_import_fu
         userdefined_table_name = "input_" + input_name + "_user_defined"
         scenario_userdefined_name = get_userdefined_scenario_settings(schema, userdefined_table_name, con)
         scenario_userdefined_value = scenario_userdefined_name['val'].values[0]
-
-        df = pd.read_csv(os.path.join(input_data_dir, input_name, scenario_userdefined_value + '.csv'), index_col=False)
         
+        df = pd.read_csv(os.path.join(input_data_dir, input_name, scenario_userdefined_value + '.csv'), index_col=False)
+
         if csv_import_function is not None:
             df = csv_import_function(df)
 
